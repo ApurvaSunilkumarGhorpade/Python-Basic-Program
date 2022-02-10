3f6f200b611f5d48d9245e07e62f391b0a714af4
ApurvaSunilkumarGhorpade <99340245+ApurvaSunilkumarGhorpade@users.noreply.github.com>
2022-02-10 10:40:24 +0530

    Add files via upload

---
+++ Arithmatic opration.py
@@ -0,0 +1,98 @@
+## Addition ##
+
+1] Integer number
+>>>12+13
+   25
+>>>-12+(-13)
+   -25
+   
+2] Float number
+>>>12.5+13.9
+   26.4
+>>>-12.5+(-13.9)
+   -26.4
+
+3] Variable
+>>>x=19
+>>>y=10
+>>>x+y
+   29
+   
+>>>_+y    ## " _ " means previous output ##
+   39
+
+## Substraction ##
+
+
+1] Integer number
+>>>12-10
+   2
+>>>-12-(-13)
+   1
+   
+2] Float number
+>>>12.5-13.9
+   -1.4000000000000004
+>>>-12.5-(-1)
+   -11.5
+
+3] Variable
+>>>x=19
+>>>y=10
+>>>x-y
+   9
+   
+>>>_-y                  ## " _ " means previous output ##
+   -1
+
+## Multiplication ##
+
+
+1] Integer number
+>>>12*10
+   120
+>>>-12*(-13)
+   156
+   
+2] Float number
+>>>12.5*13.9
+   173.75
+>>>-12.5*2
+   -25.0
+
+3] Variable
+>>>x=19
+>>>y=10
+>>>x*y
+   190
+   
+>>>_*y                ## " _ " means previous output ##
+   1900 
+
+## Division ##
+
+>>>12/10             / ->  will give you decimal number 
+   1.2               ## A Floating point number ##
+ 
+>>>12//10            // -> will give an integer value
+   1                 ## an integer number ##
+ 
+>>>12%10             % -> will give you Reminder 
+   2
+   
+>>>7/3
+   2.333333333
+
+>>>7//3
+   2
+
+>>>-7/3
+   -2.333333333
+
+>>>-7//3
+   -3                 ## lowest number in that range ##
+
+   
+   
+   
+
